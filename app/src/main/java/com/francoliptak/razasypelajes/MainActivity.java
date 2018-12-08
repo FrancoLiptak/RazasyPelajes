b4c2891a51e64069d559065eae1def6431fe91a2
Franco Liptak <franco.liptak@despegar.com>
2018-12-08 10:59:05 -0300

    textos

--- app/src/main/java/com/francoliptak/razasypelajes/MainActivity.java
+++ app/src/main/java/com/francoliptak/razasypelajes/MainActivity.java
@@ -18,8 +18,10 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    /*
     public void redirectToGame(View view){
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
+    */
 }
