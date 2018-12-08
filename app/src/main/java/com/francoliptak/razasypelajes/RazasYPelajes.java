7ed66040ff3af064d5e0be1f5a692f12b619a7a5
Franco Liptak <franco.liptak@despegar.com>
2018-12-08 20:37:48 -0300

    pantalla de juego interaccion b

--- app/src/main/java/com/francoliptak/razasypelajes/RazasYPelajes.java
+++ app/src/main/java/com/francoliptak/razasypelajes/RazasYPelajes.java
@@ -1,7 +1,24 @@
 package com.francoliptak.razasypelajes;
 
+import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
 
 public class RazasYPelajes extends AppCompatActivity {
+    private List<ImageView> horsesViews;
+    private Map<String, MediaPlayer> songs;
+    private TextView horseName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
 
 }
