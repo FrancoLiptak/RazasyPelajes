54aa7043b84240e33ead1ac58f369bb9c8d01523
Franco Liptak <franco.liptak@despegar.com>
2019-02-04 21:51:20 -0300

    Funcionamiento de la configuracion

--- app/src/main/java/com/francoliptak/razasypelajes/ConfigActivity.java
+++ app/src/main/java/com/francoliptak/razasypelajes/ConfigActivity.java
@@ -8,8 +8,6 @@ import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 
-import com.francoliptak.razasypelajes.utils.GameModeCheckbox;
-
 
 public class ConfigActivity extends AppCompatActivity {
     private Switch levelSwitch;
@@ -34,26 +32,23 @@ public class ConfigActivity extends AppCompatActivity {
     private void setValues(){
         levelSwitch.setChecked(preferences.getBoolean(getString(R.string.config_preferences_level), false));
         sexSwitch.setChecked(preferences.getBoolean(getString(R.string.config_preferences_sex), true));
-        minijuegoRadio.check(preferences.getInt(getString(R.string.config_preferences_minijuego), R.id.imgWord));
+        minijuegoRadio.check(preferences.getInt(getString(R.string.config_preferences_mini_juego), R.id.imgWord));
         recognitionMode.check(preferences.getInt(getString(R.string.config_preferences_recognition_mode), R.id.listRadioButton));
-        GameModeCheckbox.check(this);
     }
 
     private void associateViewsToVariables(){
         levelSwitch = findViewById(R.id.label_switch);
         sexSwitch = findViewById(R.id.sex_witch);
-        minijuegoRadio = findViewById(R.id.minijuego_radio);
+        minijuegoRadio = findViewById(R.id.mini_juego_radio);
         recognitionMode = findViewById(R.id.recognition_mode);
-        GameModeCheckbox.associate(this);
     }
 
     public void onAccept(View view) {
         SharedPreferences.Editor editor = this.getPreferences().edit();
         editor.putBoolean(getString(R.string.config_preferences_sex), sexSwitch.isChecked());
         editor.putBoolean(getString(R.string.config_preferences_level), levelSwitch.isChecked());
-        editor.putInt(getString(R.string.config_preferences_minijuego), minijuegoRadio.getCheckedRadioButtonId());
+        editor.putInt(getString(R.string.config_preferences_mini_juego), minijuegoRadio.getCheckedRadioButtonId());
         editor.putInt(getString(R.string.config_preferences_recognition_mode), recognitionMode.getCheckedRadioButtonId());
-        editor.putStringSet(getString(R.string.config_preferences_game_mode), GameModeCheckbox.getSelectedGameModes());
 
         editor.apply();
         finish();
