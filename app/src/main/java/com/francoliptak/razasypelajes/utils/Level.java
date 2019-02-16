66b8fa18996c9075b8a06d73904cf0523cd3dd0e
Franco Liptak <franco.liptak@despegar.com>
2019-02-16 00:56:24 -0300

    Se agrega la interfaz comparable en Level

--- app/src/main/java/com/francoliptak/razasypelajes/utils/Level.java
+++ app/src/main/java/com/francoliptak/razasypelajes/utils/Level.java
@@ -3,9 +3,32 @@ package com.francoliptak.razasypelajes.utils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public abstract class Level {
+public abstract class Level implements Comparable<Level> {
     private Integer id;
     private View view;
 
     public abstract boolean playLevelAndReportSuccessOrNot(AppCompatActivity gameActivity);
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public View getView() {
+        return view;
+    }
+
+    public void setView(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public int compareTo(Level anotherLevel) {
+        if (this.getId() > anotherLevel.getId()) return 1;
+        if (this.getId() < anotherLevel.getId()) return -1;
+        return 0;
+    }
 }
